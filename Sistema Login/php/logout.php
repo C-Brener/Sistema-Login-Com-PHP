a841c135a9a2e8d42495d4b83e76ef1c36ad2fae
brenermr <brenercaique0806@gmail.com>
2021-03-20 11:31:59 -0300

    Sistema Login

--- Sistema Login/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Location: ../index.php'); // redirecionando a pagina para onde sera levada ao deslogar da sessao
-
-?>
--- Sistema Login/php/verificando_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if(!$_SESSION['usuario']){ // caso a sessão nao exista
-        header('Location: ../index.php'); // será feito o logout e redirecionara para a pagina inicial
-        exit();
-    }
-
-?>
